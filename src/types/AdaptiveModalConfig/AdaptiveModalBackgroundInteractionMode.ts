6e13da6d3bb8fc9ea066e41b1bdb14768909a960
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-01-01 05:43:47 +0800

    ⭐️ Impl: Types - Add `AdaptiveModalBackgroundInteractionMode`

---
+++ src/types/AdaptiveModalConfig/AdaptiveModalBackgroundInteractionMode.ts
@@ -0,0 +1,9 @@
+
+
+// Maps to `AdaptiveModalKeyframeConfig.BackgroundInteractionMode`
+export type AdaptiveModalBackgroundInteractionMode = 
+  // | 'default'
+  | 'automatic'
+  | 'dismiss'
+  | 'passthrough'
+  | 'ignore';
