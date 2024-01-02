79f0ca6f2169357db851dc45dd2e42359e6e6ee1
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-01-02 10:23:24 +0800

    ⭐️ Impl: Types - Add `AdaptiveModalSnapPointPreset`

---
+++ src/types/AdaptiveModalConfig/AdaptiveModalSnapPointPreset.ts
@@ -0,0 +1,7 @@
+import { ComputableLayoutPresetConfig } from "../Temp/ComputableLayoutPreset";
+import { AdaptiveModalKeyframeConfig } from "./AdaptiveModalKeyframeConfig";
+
+export type AdaptiveModalSnapPointPreset = {
+  layoutPreset: ComputableLayoutPresetConfig,
+  keyframeConfig: AdaptiveModalKeyframeConfig
+};
--- src/types/AdaptiveModalConfig/index.ts
+++ src/types/AdaptiveModalConfig/index.ts
@@ -4,5 +4,6 @@ export * from './AdaptiveModalClampingKeys';
 export * from './AdaptiveModalConfig';
 export * from './AdaptiveModalKeyframeConfig';
 export * from './AdaptiveModalSnapPointConfig';
+export * from './AdaptiveModalSnapPointPreset';
 export * from './SnapDirection';
 export * from './SnapPercentStrategy';
