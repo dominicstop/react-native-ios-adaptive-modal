f70c89a41741df739211cd9fa07e6cca16f59e4c
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-01-02 08:16:13 +0800

    💫 Update: Types - `AdaptiveModalConfig`

--- src/types/AdaptiveModalConfig/AdaptiveModalConfig.ts
+++ src/types/AdaptiveModalConfig/AdaptiveModalConfig.ts
@@ -1,26 +1,29 @@
-import { AnimationConfig, CGSize } from "react-native-ios-utilities";
+import { AnimationConfig, CGPoint } from "react-native-ios-utilities";
 
 import { SnapDirection } from "./SnapDirection";
 import { SnapPercentStrategy } from "./SnapPercentStrategy";
 import { AdaptiveModalClampingConfig } from "./AdaptiveModalClampingConfig";
 import { AdaptiveModalSnapPointConfig } from "./AdaptiveModalSnapPointConfig";
+import { AdaptiveModalSnapPointPreset } from "./AdaptiveModalSnapPointPreset";
+import { DragHandlePosition } from "./DragHandlePosition";
 
 
 export type AdaptiveModalConfig = {
   snapPoints: AdaptiveModalSnapPointConfig[];
   snapDirection: SnapDirection;
-
+  
   snapPercentStrategy?: SnapPercentStrategy;
   snapAnimationConfig?: AnimationConfig;
   entranceAnimationConfig?: AnimationConfig;
   exitAnimationConfig?: AnimationConfig;
   interpolationClampingConfig?: AdaptiveModalClampingConfig;
-  
   initialSnapPointIndex?: number;
-  dragHandleHitSlop?: CGSize;
+  undershootSnapPoint?: AdaptiveModalSnapPointPreset;
+  overshootSnapPoint?: AdaptiveModalSnapPointPreset;
+  dragHandlePosition?: DragHandlePosition;
+  dragHandleHitSlop?: CGPoint;
   modalSwipeGestureEdgeHeight?: number;
-
   shouldSetModalScrollViewContentInsets?: boolean;
   shouldSetModalScrollViewVerticalScrollIndicatorInsets?: boolean;
-  shouldSetModalScrollViewHorizontalScrollIndicatorInsets?: boolean;
+  shouldSetModalScrollViewHorizontalScrollIndicatorInsets?: boolean
 };
