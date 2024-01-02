400c7109310f66b6f6de3c8f95ebb24893ff3a25
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-01-02 09:40:15 +0800

    💫 Update: `Dictionary+Helpers`

--- ios/Temp/Dictionary+Helpers.swift
+++ ios/Temp/Dictionary+Helpers.swift
@@ -10,6 +10,35 @@ import DGSwiftUtilities
 
 // TODO: Move to `DGSwiftUtilities`
 public extension Dictionary where Key == String {
+
+  func getValueFromDictionary<T: OptionSet & InitializableFromString>(
+    forKey key: String,
+    type: T.Type = T.self
+  ) throws -> T {
+  
+    let stringValues = try self.getValueFromDictionary(
+      forKey: "modalMaskedCorners",
+      type: [String].self
+    );
+    
+    var optionSets = stringValues.compactMap {
+      try? T.init(fromString: $0);
+    };
+    
+    guard let optionSetItem = optionSets.popLast() else {
+      throw GenericError(
+        errorCode: .unexpectedNilValue,
+        description: "array of optionSet values is 0",
+        extraDebugValues: [
+          "key": key
+        ]
+      );
+    };
+    
+    return optionSets.reduce(optionSetItem) {
+      $0.union($1);
+    };
+  };
   
   func getColorFromDictionary(forKey key: String) throws -> UIColor {
     let stringValue = try? self.getValueFromDictionary(
