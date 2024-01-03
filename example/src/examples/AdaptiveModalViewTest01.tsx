e6643bf3f9dbd2fa768388b3c8d43842a1ddd2b3
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-01-03 13:39:10 +0800

    ⭐️ Impl: Add `AdaptiveModalConfigPresets`

--- example/src/examples/AdaptiveModalViewTest01.tsx
+++ example/src/examples/AdaptiveModalViewTest01.tsx
@@ -7,193 +7,18 @@ import { AdaptiveModalConfig, AdaptiveModalView } from 'react-native-ios-adaptiv
 import type { ExampleItemProps } from './SharedExampleTypes';
 import { ExampleItemCard } from '../components/ExampleItemCard';
 import { CardButton } from '../components/Card';
+import { AdaptiveModalConfigPresets } from '../constants/AdaptiveModalConfigPresets';
 
-const ScreenSizes: Record<string, CGSize> = {
-  iPhone8: {
-    width: 375, 
-    height: 667
-  },
-};
-
-const modalConfig: AdaptiveModalConfig = {
-  snapPoints: [{
-    // Snap Point 1
-    mode: 'standard',
-    layoutConfig: {
-      horizontalAlignment: 'center',
-      verticalAlignment: 'bottom',
-      width: {
-        mode: {
-          mode: 'stretch',
-        },
-      },
-      height: {
-        mode: {
-          mode: 'percent',
-          percentValue: 0.3,
-        },
-      }
-    },
-    keyframeConfig: {
-      modalShadowOffset: {
-        width: 0,
-        height: -2,
-      },
-      modalShadowOpacity: 0.2,
-      modalShadowRadius: 7,
-      modalCornerRadius: 25,
-      modalMaskedCorners: ['topCorners'],
-      modalBackgroundOpacity: 0.9,
-      modalBackgroundVisualEffect: 'systemUltraThinMaterial',
-      modalBackgroundVisualEffectIntensity: 1,
-      backgroundOpacity: 0,
-      backgroundVisualEffect: 'systemUltraThinMaterialDark',
-      backgroundVisualEffectIntensity: 0
-    },
-  }, {
-    // Snap Point 2
-    mode: 'standard',
-    layoutConfig: {
-      horizontalAlignment: 'center',
-      verticalAlignment: 'bottom',
-      width: {
-        mode: {
-          mode: 'stretch'
-        },
-      },
-      height: {
-        mode: {
-          mode: 'percent',
-          percentValue: 0.5,
-        },        
-      },
-      marginLeft: {
-        mode: {
-          mode: 'constant',
-          value: 15,
-        }
-      },
-      marginRight: {
-        mode: {
-          mode: 'constant',
-          value: 15,
-        }
-      },
-      marginBottom: {
-        mode: {
-          mode: 'safeAreaInsets',
-          insetKey: 'bottom',
-        },
-        minValue: {
-          mode: 'constant',
-          value: 15
-        }
-      },
-    },
-    keyframeConfig: {
-      secondaryGestureAxisDampingPercent: 1,
-      modalShadowOffset: {
-        width: 2, 
-        height: 2
-      },
-      modalShadowOpacity: 0.2,
-      modalShadowRadius: 15,
-      modalCornerRadius: 10,
-      modalMaskedCorners: ['allCorners'],
-      modalBackgroundOpacity: 0.85,
-      modalBackgroundVisualEffectIntensity: 0.6,
-      backgroundOpacity: 0.1,
-      backgroundVisualEffectIntensity: 0.075
-    },
-  }, {
-    // Snap Point 3
-    mode: 'standard',
-    layoutConfig: {
-      horizontalAlignment: 'center',
-      verticalAlignment: 'center',
-      width: {
-        mode: {
-          mode: 'percent',
-          percentValue: 0.85,
-        },
-        maxValue: {
-          mode: 'constant',
-          value: ScreenSizes.iPhone8.width,
-        }
-      },
-      height: {
-        mode: {
-          mode: 'percent',
-          percentValue: 0.75,
-        },
-        maxValue: {
-          mode: 'constant',
-          value: ScreenSizes.iPhone8.height,
-        }
-      }
-    },
-    keyframeConfig: {
-      secondaryGestureAxisDampingPercent: 1,
-      modalShadowOffset: {
-        width: 0, 
-        height: -1
-      },
-      modalShadowOpacity: 0.4,
-      modalShadowRadius: 10,
-      modalCornerRadius: 25,
-      modalMaskedCorners: ['allCorners'],
-      modalBackgroundOpacity: 0.83,
-      modalBackgroundVisualEffectIntensity: 1,
-      backgroundVisualEffectIntensity: 1
-    },
-  }, {
-    // Snap Point 4
-    mode: 'standard',
-    layoutConfig: {
-      horizontalAlignment: 'center',
-      verticalAlignment: 'bottom',
-      width: {
-        mode: {
-          mode: 'stretch'
-        },
-      },
-      height: {
-        mode: {
-          mode: 'stretch'
-        },
-      },
-      marginTop: {
-        mode: {
-          mode: 'safeAreaInsets',
-          insetKey: 'top'
-        },
-      },
-    },
-    keyframeConfig: {
-      secondaryGestureAxisDampingPercent: 1,
-      modalShadowOffset: {
-        width: 0, 
-        height: -1
-      },
-      modalShadowOpacity: 0.4,
-      modalShadowRadius: 10,
-      modalCornerRadius: 25,
-      modalMaskedCorners: ['topCorners'],
-      modalBackgroundOpacity: 0.83,
-      modalBackgroundVisualEffectIntensity: 1,
-      backgroundVisualEffectIntensity: 1
-    },
-  }],
-  snapDirection: 'bottomToTop',
-  overshootSnapPoint: {
-    layoutPreset: {
-      mode: 'preset',
-      preset: 'fitScreen'
-    },
-  },
-};
 
 export function AdaptiveModalViewTest01(props: ExampleItemProps) {
+  const [modalConfigPresetCounter, setModalConfigPresetCounter] = React.useState(0);
+
+  const modalConfigPresetIndex = 
+    modalConfigPresetCounter % AdaptiveModalConfigPresets.length;
+  
+  const currentModalConfigPreset = 
+    AdaptiveModalConfigPresets[modalConfigPresetIndex];
+
   const modalRef = React.createRef<AdaptiveModalView>();
   
   return (
@@ -202,20 +27,28 @@ export function AdaptiveModalViewTest01(props: ExampleItemProps) {
       title={'AdaptiveModalViewTest01'}
       subtitle={'TBA'}
       description={[
-        `Test - TBA`
+        `Test - TBA`,
+        `Current Modal Config Index: ${modalConfigPresetIndex}`,
       ]}
     >
       <AdaptiveModalView
         ref={modalRef}
-        modalConfig={modalConfig}
+        modalConfig={currentModalConfigPreset}
       >
         <View style={styles.rootModalContainer}>
           <Text>"Adaptive Modal"</Text>
         </View>
       </AdaptiveModalView>
       <CardButton
+        title={'Next Modal Config'}
+        subtitle={'Cycle to next modal config preset...'}
+        onPress={() => {
+          setModalConfigPresetCounter(prev => prev + 1);
+        }}
+      />
+      <CardButton
         title={'Show Modal'}
-        subtitle={'Present modal...'}
+        subtitle={'Present modal using modal config preset...'}
         onPress={() => {
           modalRef?.current?.presentModal();
         }}
