961cc81f9ec57f2c1b09f507aec3c6d7fe9631dd
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-01-14 07:32:09 +0800

    💫 Update: Ex - Reorder `RNIModalContentAnchorModes`

--- example/src/constants/RNIModalContentAnchorModes.ts
+++ example/src/constants/RNIModalContentAnchorModes.ts
@@ -1,6 +1,6 @@
 import { RNIModalContentAnchorMode } from "react-native-ios-adaptive-modal";
 
 export const RNIModalContentAnchorModes: RNIModalContentAnchorMode[] = [
-  'center',
   'stretch',
+  'center',
 ];
