30e7039e9edbbe7c684c25063ac5fa619df9e3df
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-02-04 01:35:53 +0800

    💫 Update: Ex - `AdaptiveModalViewTest03`

--- example/src/examples/AdaptiveModalViewTest03/ModalContent.tsx
+++ example/src/examples/AdaptiveModalViewTest03/ModalContent.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, ScrollView, FlatList, ListRenderItem } from 'react-native';
+import { StyleSheet, ScrollView, ListRenderItem } from 'react-native';
 
 import { EventItem } from './SharedTypes';
 import { ModalEventListItem } from './ModalEventListItem';
@@ -9,40 +9,29 @@ export function ModalContent(props: {
   eventItems: Array<EventItem>;
 }){
 
-  const hasItems = (props?.eventItems?.length ?? 0) > 0;
-  
-  const renderItem: ListRenderItem<EventItem> = ({ item }) => (
-    <ModalEventListItem
-      eventItem={item}
-    />
-  );
-
   return (
     <ScrollView
       style={styles.listOuter}
       contentContainerStyle={styles.listContentContainer}
       horizontal={false}
-    >
-      <FlatList
-        data={props.eventItems}
-        style={styles.listInner}
-        renderItem={renderItem}
-        keyExtractor={(item) => `item-${item.id}`}
-        nestedScrollEnabled={true}
-      />
+    > 
+      {props.eventItems.map((item, index) => (
+        <ModalEventListItem
+          key={item.timestamp}
+          eventItem={item}
+        />
+      ))}
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   listOuter: {
-    paddingTop: 24,
+    paddingTop: 30,
   },
   listContentContainer: {
     paddingLeft: 14,
     paddingRight: 14,
-  },
-  listInner: {
-    paddingBottom: 80
+    paddingBottom: 120
   },
 });
